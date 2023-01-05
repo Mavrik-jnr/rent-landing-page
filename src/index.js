a6c5473aa09749e14c9057ea3ba5ecfb04670cc7
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2023-01-05 11:33:07 +0100

    chore: set up mui theming and react router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
+
+/*Todo
+0. Set up themes for type, colors and shadows
+1. Set up Box component for page sections
+2. Set up Card Variants
+3. 
+*/
+const theme = createTheme({
+  palette: {
+    primary: { main: "#f4511e" },
+    white: "#FFFFF",
+    background: { secondary: "#e6e6e6" },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
